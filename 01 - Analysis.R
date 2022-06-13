783b4cc574b646cd16e83437201dbe718c974215
Cristian Opariuc-Dan <copariuc@gmail.com>
2022-06-13 12:33:27 +0300

    Setting up analysis template

--- 01 - Analysis.R
+++ 01 - Analysis.R
@@ -1,20 +1,20 @@
 # Installing and loading libraries
 if(!require(ggplot2)) install.packages("ggplot2")
-if(!require(scale)) install.packages("scale")
+if(!require(dplyr)) install.packages("dplyr")
+if(!require(memisc)) install.packages("memisc")
 
-library(ggplot2); library(scale)
+library(ggplot2); library(dplyr)
 
 # Loading dataset ####
-load("Date.RData")
+load("Date.RData"); names(ds)
 
-# Analyzing Region vs Item 1 ####
-chi.R.I1 <- chisq.test(ds$i1, ds$rang,  simulate.p.value = T); chi.R.I1
-F.R.I1 <- fisher.test(ds$rang, ds$i1, simulate.p.value = T); F.R.I1
-tabCont <- table(ds$rang, ds$i1)
-mosaicplot(tabCont, shade = TRUE,
-           main = "Contingenta: - Locality rank vs. Normative documents")
+# Analyzing Region ####
+## Item 1 
+chi.R.I1 <- chisq.test(ds$i1, ds$rang); chi.R.I1
+mosaicplot(chi.R.I1$observed, shade = TRUE,
+           main = "Contingency: - Locality rank vs. Normative documents",
+           xlab = "Normative documents", ylab = "Rank of locality")
+R.I.1.prop <- round(prop.table(chi.R.I1$observed) * 100, 2); R.I.1.prop
 ggplot(data = ds, mapping = aes(x = rang, fill = i1)) + 
   geom_bar(position = "dodge") + xlab("Locality rank") + 
   ylab("Frequency") + scale_fill_discrete(name = "Normative \ndocuments")
-
-
